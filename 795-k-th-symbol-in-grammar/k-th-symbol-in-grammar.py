bcb3b19017f13a3868318cdc893f841d4767be46
Nandini Sangu <nandinisangu7@gmail.com>
2026-08-15 19:27:57 +0530

    Time: 4 ms (1.23%) | Memory: 19.3 MB (20.59%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -0,0 +1,17 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if(n==1 and k==1):
+            return 0
+        length=2**(n-1)
+        mid=length//2
+        if k<=mid:
+            return self.kthGrammar(n-1,k)
+        else:
+            return 1-self.kthGrammar(n-1,k-mid)
+
+
+
+
+
+            
+        
